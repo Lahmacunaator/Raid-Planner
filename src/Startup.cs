13e6c95bee2e2c0ec03ba9e45ba89a5151e108ea
Barış Can Soy <43369191+Lahmacunaator@users.noreply.github.com>
2022-11-09 11:09:51 +0200

    added project to git

---
+++ src/Startup.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using BlazorLoginDiscord.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Discord.OAuth2;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace BlazorLoginDiscord
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddHttpContextAccessor();
+            services.AddSingleton<UserService>();
+
+            //Configure authentication for the user
+            services.AddAuthentication(opt =>
+                {
+                    opt.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    opt.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    opt.DefaultChallengeScheme = DiscordDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddDiscord(x =>
+                {
+                    x.AppId = Configuration["Discord:AppId"];
+                    x.AppSecret = Configuration["Discord:AppSecret"];
+                    x.Scope.Add("identify");
+
+                    //Required for accessing the oauth2 token in order to make requests on the user's behalf, ie. accessing the user's guild list
+                    x.SaveTokens = true;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            var provider = new FileExtensionContentTypeProvider();
+            provider.Mappings.Remove(".data");
+            provider.Mappings[".data"] = "application/octet-stream";
+            provider.Mappings.Remove(".wasm");
+            provider.Mappings[".wasm"] = "application/wasm";
+            provider.Mappings.Remove(".symbols.json");
+            provider.Mappings[".symbols.json"] = "application/octet-stream";
+            app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = provider });
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
